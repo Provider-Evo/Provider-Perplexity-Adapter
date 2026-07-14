540c9c489b1d286ed5261fca19fd742d5047a082
nichengfuben <nichengfuben@outlook.com>
2026-07-14 21:55:21 +0800

    chore: initial import

---
+++ provider_perplexity/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+# src/platforms/perplexity/__init__.py
+from provider_perplexity.adapter import Adapter, PerplexityAdapter
+
+__all__ = ["PerplexityAdapter", "Adapter"]
